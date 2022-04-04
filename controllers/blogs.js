c433e0a9cc73f6b809c33d5011b37d733e231f68
Petrus Hurtig <petrushurtig@hotmail.com>
2022-04-04 15:17:41 +0300

    4.2.

---
+++ controllers/blogs.js
@@ -0,0 +1,42 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs)
+    })
+})
+
+blogsRouter.get('/:id', (req, res, next) => {
+    Blog.findById(req.params.id)
+    .then(blog => {
+       if (blog) {
+           res.json(blog)
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (req, res, next) => {
+    const body = req.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+    blog.save()
+    .then(savedBlog => {
+        res.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+//delete
+
+//put
+
+module.exports = blogsRouter
--- index.js
+++ index.js
@@ -1,60 +1,27 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { nextTick } = require('process')
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-//Mongo
-const mongoUrl = process.env.MONGO_URL
-console.log('connectiong to', mongoUrl)
-mongoose.connect(mongoUrl)
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
-const Blog = mongoose.model('Blog', blogSchema)
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-//app use
-app.use(express.json())
-app.use(cors())
+const server = http.createServer(app)
 
 //Routes
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
